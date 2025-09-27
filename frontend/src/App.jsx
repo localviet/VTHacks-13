06059ac4ef2178d22b51fa575d1b4a37fedcfa42
minjoon0308 <minjoon0308@gmail.com>
2025-09-27 19:56:54 -0400

    signin & signout buttons change based on login status

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,22 @@
 //User Pages
-import { useState } from 'react'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import SignUpPage from './pages/SignUp.jsx'
-import SignInPage from './pages/SignIn.jsx'
-import Dashboard from './pages/Dashboard.jsx'
-import Search from './pages/Search.jsx'
-import Settings from './pages/Settings.jsx'
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import SignUpPage from "./pages/SignUp.jsx";
+import SignInPage from "./pages/SignIn.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Search from "./pages/Search.jsx";
+import Settings from "./pages/Settings.jsx";
 
 //Business
-import DashboardBusiness from './pages/Business/DashboardBusiness.jsx'
-import BusinessSignUp from './pages/Business/BusinessSignUp.jsx'
-import BusinessSearch from './pages/Business/BusinessSearch.jsx'
-import BusinessSettings from './pages/Business/BusinessSettings.jsx'
+import DashboardBusiness from "./pages/Business/DashboardBusiness.jsx";
+import BusinessSignUp from "./pages/Business/BusinessSignUp.jsx";
+import BusinessSearch from "./pages/Business/BusinessSearch.jsx";
+import BusinessSettings from "./pages/Business/BusinessSettings.jsx";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <Router>
@@ -32,11 +31,10 @@ function App() {
           <Route path="business/dashboard" element={<DashboardBusiness />} />
           <Route path="business/search" element={<BusinessSearch />} />
           <Route path="business/setting" element={<BusinessSettings />} />
-
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
